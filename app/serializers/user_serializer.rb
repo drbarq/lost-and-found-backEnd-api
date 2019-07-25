b3b1e3262cf487fc5df3712573504e4a29264f45
drbarq <J.Tustin@gmail.com>
2019-07-25 10:44:10 -0600

    cleaned up hash

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,9 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :contact_number, :email_address, :items
-  # is it possible to create an object and add it in?  
+ 
   def items 
-
-    # nested object 
     self.object.items.map  do |item|
       {id: item.id,
         name: item.item_name,
@@ -21,53 +19,3 @@ class UserSerializer < ActiveModel::Serializer
       end 
     end  
 end 
-
-
-#     def items 
-#       self.object.items.map  do |item|
-#         phone = item.contacts.select do |contact|
-#                   contact.contactable_type == "Phone"
-#                 end[0]
-#         contact = phone.contactable
-#             # byebug
-
-
-#         hash = {id: item.id,
-#               name: item.item_name,
-
-#               email: contact
-
-#             }
-#       end 
-#     end
-# end 
-
-# # elsif contact.contactable_type == "Phone"
-# #   phone: contact.contactable.phone_number
-# # elsif contact.contactable_type == "Text"
-# #   text: contact.contactable.text_number
-
-
-#   # def items 
-#   #   self.object.items.map  do |item|
-#   #     @comms = item.contacts.map do |contact|
-#   #               if contact.contactable_type == "Email"
-#   #                email: contact.contactable.email_address
-#   #               elsif contact.contactable_type == "Phone"
-#   #                 return phone: contact.contactable.phone_number
-#   #               elsif contact.contactable_type == "Text"
-#   #                 (text: contact.contactable.text_number)
-#   #               end 
-#   #             end
-
-
-#   #     {id: item.id,
-#   #       name: item.item_name,
-#   #       @comms 
-#   #     }
-
-
-#   #     end 
-#   #   end 
-
-
