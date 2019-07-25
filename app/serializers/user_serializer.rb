bd6f431d1349386e0be8eb1c34ff7d4a0748974a
drbarq <J.Tustin@gmail.com>
2019-07-25 10:43:42 -0600

    added contact hash

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,22 +1,73 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :contact_number, :email_address, :items
-
+  # is it possible to create an object and add it in?  
   def items 
+
+    # nested object 
     self.object.items.map  do |item|
       {id: item.id,
         name: item.item_name,
-        contact: item.contacts.map do |contact|
+        contact: item.contacts.inject({}) do |k, contact|          
           if contact.contactable_type == "Email"
-            {email: contact.contactable.email_address}
+            k[:email] = contact.contactable.email_address
           elsif contact.contactable_type == "Phone"
-            {phone: contact.contactable.phone_number}
+            k[:phone] = contact.contactable.phone_number
           elsif contact.contactable_type == "Text"
-            {text: contact.contactable.text_number}
-          end 
+            k[:text] = contact.contactable.text_number
+          end
+          k 
         end
       }
       end 
-    end 
+    end  
+end 
 
 
-end
+#     def items 
+#       self.object.items.map  do |item|
+#         phone = item.contacts.select do |contact|
+#                   contact.contactable_type == "Phone"
+#                 end[0]
+#         contact = phone.contactable
+#             # byebug
+
+
+#         hash = {id: item.id,
+#               name: item.item_name,
+
+#               email: contact
+
+#             }
+#       end 
+#     end
+# end 
+
+# # elsif contact.contactable_type == "Phone"
+# #   phone: contact.contactable.phone_number
+# # elsif contact.contactable_type == "Text"
+# #   text: contact.contactable.text_number
+
+
+#   # def items 
+#   #   self.object.items.map  do |item|
+#   #     @comms = item.contacts.map do |contact|
+#   #               if contact.contactable_type == "Email"
+#   #                email: contact.contactable.email_address
+#   #               elsif contact.contactable_type == "Phone"
+#   #                 return phone: contact.contactable.phone_number
+#   #               elsif contact.contactable_type == "Text"
+#   #                 (text: contact.contactable.text_number)
+#   #               end 
+#   #             end
+
+
+#   #     {id: item.id,
+#   #       name: item.item_name,
+#   #       @comms 
+#   #     }
+
+
+#   #     end 
+#   #   end 
+
+
