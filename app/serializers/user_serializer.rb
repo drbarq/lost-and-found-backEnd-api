d82661665010da9ce05315744951bf54d68b962c
drbarq <J.Tustin@gmail.com>
2019-07-25 11:09:18 -0600

    added comm id

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -7,10 +7,13 @@ class UserSerializer < ActiveModel::Serializer
         name: item.item_name,
         contact: item.contacts.inject({}) do |k, contact|          
           if contact.contactable_type == "Email"
+            k[:emailID] = contact.contactable.id
             k[:email] = contact.contactable.email_address
           elsif contact.contactable_type == "Phone"
+            k[:phoneID] = contact.contactable.id
             k[:phone] = contact.contactable.phone_number
           elsif contact.contactable_type == "Text"
+            k[:textID] = contact.contactable.id
             k[:text] = contact.contactable.text_number
           end
           k 
